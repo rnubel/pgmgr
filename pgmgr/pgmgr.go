ecfe10a426e240d060140a8b02fa064977fce4a8
Robert Nubel <robert.nubel@gmail.com>
2015-05-21 23:42:46 -0500

    Builds out Dump, Load, and Drop functionality within the pgmgr subpackage.

--- pgmgr/pgmgr.go
+++ pgmgr/pgmgr.go
@@ -5,8 +5,32 @@ import (
 	"os/exec"
 )
 
-func Create(dbname string) error {
-	return sh("createdb", []string{dbname})
+type Config struct {
+	// connection
+	Username	string
+	Password	string
+	Database  string
+	Host			string
+	Port			int
+
+	// filepaths
+	DumpFile  string
+}
+
+func Create(c *Config) error {
+	return sh("createdb", []string{c.Database})
+}
+
+func Drop(c *Config) error {
+	return sh("dropdb", []string{c.Database})
+}
+
+func Dump(c *Config) error {
+	return sh("pg_dump", []string{"-f", c.DumpFile})
+}
+
+func Load(c *Config) error {
+	return sh("psql", []string{"-f", c.DumpFile})
 }
 
 func sh(command string, args []string) error {
--- pgmgr/pgmgr_test.go
+++ pgmgr/pgmgr_test.go
@@ -4,11 +4,22 @@ import (
 	"testing"
 	"../pgmgr"
 	"os/exec"
+	"io/ioutil"
+	"strings"
 )
+
+func globalConfig() *pgmgr.Config {
+	return &pgmgr.Config{
+		Database: "testdb",
+		Host:			"localhost",
+		Port:			5432,
+		DumpFile: "/tmp/dump.sql",
+	}
+}
 
 func TestCreate(t *testing.T) {
 	sh(t, "dropdb", []string{"testdb"})
-	err := pgmgr.Create("testdb")
+	err := pgmgr.Create(globalConfig())
 
 	if err != nil {
 		t.Log(err)
@@ -18,6 +29,66 @@ func TestCreate(t *testing.T) {
 	// if we can't remove that db, it couldn't have been created by us above.
 	if err = sh(t, "dropdb", []string{"testdb"}); err != nil {
 		t.Fatal("database doesn't seem to have been created!")
+	}
+}
+
+func TestDrop(t *testing.T) {
+	sh(t, "createdb", []string{"testdb"})
+	err := pgmgr.Drop(globalConfig())
+
+	if err != nil {
+		t.Log(err)
+		t.Fatal("Could not drop database")
+	}
+
+	if err = sh(t, "createdb", []string{"testdb"}); err != nil {
+		t.Fatal("database doesn't seem to have been dropped!")
+	}
+}
+
+func TestDump(t *testing.T) {
+	sh(t, "dropdb", []string{"testdb"})
+	sh(t, "createdb", []string{"testdb"})
+	sh(t, "psql", []string{"-c","CREATE TABLE bars (bar_id INTEGER);"})
+
+	err := pgmgr.Dump(globalConfig())
+
+	if err != nil {
+		t.Log(err)
+		t.Fatal("Could not dump database to file")
+	}
+
+	file, err := ioutil.ReadFile("/tmp/dump.sql")
+	if err != nil {
+		t.Log(err)
+		t.Fatal("Could not read dump")
+	}
+
+	if !strings.Contains(string(file), "CREATE TABLE bars") {
+		t.Fatal("dump does not contain the table definition")
+	}
+}
+
+func TestLoad(t *testing.T) {
+	sh(t, "dropdb", []string{"testdb"})
+	sh(t, "createdb", []string{"testdb"})
+
+	ioutil.WriteFile("/tmp/dump.sql", []byte(`
+		CREATE TABLE foos (foo_id INTEGER);
+		INSERT INTO foos (foo_id) VALUES (1), (2), (3);
+	`), 0644)
+
+	err := pgmgr.Load(globalConfig())
+
+	if err != nil {
+		t.Log(err)
+		t.Fatal("Could not load database from file")
+	}
+
+	err = sh(t, "psql", []string{"-c","SELECT * FROM foos;"})
+	if err != nil {
+		t.Log(err)
+		t.Fatal("Could not query the table; schema didn't load, probably")
 	}
 }
 
